279b6527c76f380ae2b21fdd6552201d0c81ec78
liPatrick <patrick8289@gmail.com>
2015-06-06 11:36:30 -0700

    Initial Commit

---
+++ AudioVersion5/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AudioVersion5
+//
+//  Created by Patrick Li on 6/6/15.
+//  Copyright (c) 2015 Dali Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
